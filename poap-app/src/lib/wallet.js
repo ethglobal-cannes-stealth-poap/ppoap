0094133492bf5f7f261849b2dd9b87d8fdc8522e
gedithejedi <33606955+gedithejedi@users.noreply.github.com>
2025-07-05 01:13:39 +0200

    feat: add veim client call code

--- poap-app/src/lib/wallet.js
+++ poap-app/src/lib/wallet.js
@@ -8,3 +8,51 @@ export const config = createConfig({
     [sepolia.id]: http(),
   },
 });
+
+/* 
+
+ How to use this in your code:
+ 
+  fetchEventsViem({
+    client: client,
+    address: contractAddress,
+    event: stakedEvent,
+  })
+
+*/
+
+export const getViemClient = () => {
+  const client = createPublicClient({
+    chain: sepolia,
+    transport: http(),
+    batch: {
+      multicall: !!viemChain.contracts?.multicall3?.address
+    }
+  });
+
+  return client;
+};
+
+export const fetchEventsViem = async ({
+  client,
+  address,
+  event,
+  args,
+}) => {
+  try {
+    return await client.getLogs({
+      address,
+      event,
+      args: args,
+    });
+  } catch (error) {
+    logger.error("Onchain event fetch failed", error);
+    throw error;
+  }
+}
+
+export const getEvent = (abi, eventName) => {
+  return (abi).find((item) => {
+    return item.type === "event" && item?.name === eventName;
+  });
+}
